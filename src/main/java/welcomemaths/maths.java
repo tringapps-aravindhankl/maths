d8860ca78e94314030fee811a71907951ba015da
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-02-21 10:00:08 +0530

    Update maths.java

--- src/main/java/welcomemaths/maths.java
+++ src/main/java/welcomemaths/maths.java
@@ -1,47 +1,49 @@
 package welcomemaths;
 import java.util.*;
 import java.util.logging.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-class Graph implements cloneable {
-    Integer coordinatex;
-    Integer coordinatey;
-    Logger log=Logger.getLogger("hi");
+class Graph implements Cloneable {
+	    Integer coordinatex;
+	    Integer coordinatey;
+	    Logger log=Logger.getLogger("hi");
 
-    Graph(Integer a, Integer b) {
-       coordinatex = a;
-       coordinatey= b;
-    }
+	    Graph(Integer a, Integer b) {
+	       coordinatex = a;
+	       coordinatey= b;
+	    }
 
-    void active() {
-        Boolean c = coordinatex.equals(coordinatey);
-        log.info(c);
-    }
-    Graph helo()
-    {
-        try
-        {
-            return (Graph) super.clone();
-        }
-        catch (CloneNotSupportedException e)
-        {
-            log.info("cloned error");
-            return this;
-        }
-    }
-}
+	    void active() {
+	        boolean condition = coordinatex.equals(coordinatey);
+	        log.log(Level.INFO,()->" "+condition);
+	    }
+	    Graph helo()
+	    {
+	        try
+	        {
+	            return (Graph) super.clone();
+	        }
+	        catch (CloneNotSupportedException e)
+	        {
+	            log.info("cloned error");
+	            return this;
+	        }
+	    }
+	}
+
 
 public class Maths
 {
-    private static Scanner scan;
-
-	public static void main( String[] args ){
-    	scan = new Scanner(System.in);
-	Logger log=Logger.getLogger("hi");
+	public static void main( String[] args )
+	{
+    	Scanner scan = new Scanner(System.in);
+	    Logger log=Logger.getLogger("hi");
         log.info("enter x coordinate");
-        Integer a = scan.nextInt();
+        Integer firstcoordinate = scan.nextInt();
         log.info("Enter y coordinate");
-        Integer b = scan.nextInt();
-        Graph s1 = new Graph(a, b);
+        Integer secondcoordinate = scan.nextInt();
+        Graph s1 = new Graph(firstcoordinate, secondcoordinate);
         Graph s2 = s1.helo();
         s2.active();
     }
