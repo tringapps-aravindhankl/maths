4901ea02d9c3daf21f866b3fa825b5fc00f01aa2
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-02-26 01:34:17 +0530

    Update maths.java

--- src/main/java/welcomemaths/maths.java
+++ src/main/java/welcomemaths/maths.java
@@ -1,37 +1,34 @@
 package welcomemaths;
 import java.util.*;
+
 import java.util.logging.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+class Graph implements Cloneable {
+    Integer NumberX;
+    Integer NumberY;
+    Logger log=Logger.getLogger("hi");
 
-class Graph implements cloneable {
-	    Integer coordinatex;
-	    Integer coordinatey;
-	    Logger log=Logger.getLogger("hi");
+    Graph(Integer FirstNumber, Integer SecondNumber) {
+       this.NumberX= FirstNumber;
+       this.NumberY= SecondNumber;
+    }
 
-	    Graph(Integer a, Integer b) {
-	       coordinatex = a;
-	       coordinatey= b;
-	    }
-
-	    void active() {
-	        boolean condition = coordinatex.equals(coordinatey);
-	        log.log(Level.INFO,()->"Answer:"+condition);
-	    }
-	    Graph helo()
-	    {
-	        try
-	        {
-	            return (Graph) super.clone();
-	        }
-	        catch (CloneNotSupportedException e)
-	        {
-	            log.info("cloned error");
-	            return this;
-	        }
-	    }
-	}
-
+    void active() {
+        boolean condition = NumberX.equals(NumberY);
+        log.log(Level.INFO,()->" "+condition);
+    }
+    Graph helo()
+    {
+        try
+        {
+            return (Graph) super.clone();
+        }
+        catch (CloneNotSupportedException e)
+        {
+            log.info("Cloned error");
+            return this;
+        }
+    }
+}
 
 public class Maths
 {
@@ -44,7 +41,7 @@ public class Maths
         log.info("Enter y coordinate");
         Integer secondcoordinate = scan.nextInt();
         Graph s1 = new Graph(firstcoordinate, secondcoordinate);
-        Graph s2 = s1.helo();
+        Graph s2 = s1.helo(); //clone(copy of an object)
         s2.active();
     }
 }
